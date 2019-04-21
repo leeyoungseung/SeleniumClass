3be228d4c0e919a70e456f04153060e41690780c
leeyoungseung <leeyoungseung@DESKTOP-QG5IT2R>
2019-04-21 10:29:28 +0900

    class 1 commit 3

--- src/test/java/selenium/test/web/lesson01/Class_A_01.java
+++ src/test/java/selenium/test/web/lesson01/Class_A_01.java
@@ -31,8 +31,6 @@ public class Class_A_01 {
 				
 		//웹브라우저 창 사이즈
 		driver.manage().window().setSize(new Dimension(1024, 768));
-		
-		
 	}
 	
 	//슬립 함수
@@ -123,90 +121,6 @@ public class Class_A_01 {
 		for (int i=0; i<res.length; i++) {
 			System.out.println("결과 확인 "+(i+1)+"번 째 값은 : ["+ res[i] +"]");
 		}
-		
-	}
-	
-	@Test
-	public void Test003() throws IOException, WebDriverException{
-		//드라이버 시작
-		if(driver==null) {
-			startDriver();
-		}
-		//이동
-		driver.get("http://127.0.0.1/seleniumTest/Class_A_03.html");
-		
-		//findElements메소드는 지정한 값에 해당하는 WebElement의 List를 반환한다.
-		List<WebElement> welist = driver.findElements(By.tagName("th"));
-		
-		System.out.println("몇개의 요소가 있는지 : [ "+ welist.size()+" ]");
-		
-		//list안의 WebElement에서 지정하고 싶은 WebElement를 다음과 같은 방법으로 지정할 수 있다.
-		WebElement selectedWel = null;
-		for(WebElement wel : welist) {
-			if(wel.getText().equals("Lastname")) {
-				selectedWel = wel;
-			}
-			System.out.println(wel.getText());
-		}
-		System.out.println("★★★★★★★★★★★★★★[ "+selectedWel.getText()+" ]★★★★★★★★★★★★★★");
-		
-		WebElement webElement = driver.findElement(By.xpath("/html/body/div/table"));
-		
-		//지정한 WebElement의 자식 WebElement를 findElements메소드로 지정할 수 있다.
-		List<WebElement> welist2 = webElement.findElements(By.tagName("tr"));
-		System.out.println("테이블의 tr자식 요소는 몇개인지 : [ "+welist2.size()+" ]");
-		for(WebElement wel : welist2) {
-			//tr에는 text값이 없지만 자식인 td에 text값이 있으므로 그것을 출력함
-			System.out.println(wel.getText());
-		}
-		
-		List<WebElement> welist3 = webElement.findElements(By.tagName("td"));
-		System.out.println("테이블의 td자식 요소는 몇개인지 : [ "+welist3.size()+" ]");
-		for(WebElement wel : welist3) {
-			System.out.println(wel.getText());
-		}
-	}
-	
-	@Test
-	public void Test004() throws IOException, WebDriverException{
-		//드라이버 시작
-		if(driver==null) {
-			startDriver();
-		}
-		//이동
-		driver.get("http://127.0.0.1/seleniumTest/Class_A_03.html");
-		
-		//findElements메소드는 지정한 값에 해당하는 WebElement의 List를 반환한다.
-		WebElement wel = driver.findElement(By.tagName("table"));
-		//WebElement wel2 = driver.findElement(By.className("table table-bordered")); -> 중간에 뛰어쓰기가 있는 것은 인식을 못함ㄷㄷ
-		System.out.println(wel.getAttribute("class"));
-		
-		//선택한 WebElement의 부모 요소를 지정하기
-		//참조 : https://stackoverflow.com/questions/24267413/how-to-get-parent-of-webelement
-		// xpath를 이용한 방법
-		WebElement parentElement = wel.findElement(By.xpath("./.."));
-		System.out.println("부모의 태그 값 : [ "+ parentElement.getTagName() +" ]");
-		System.out.println("부모의 속성 값 : [ "+ parentElement.getAttribute("class") +" ]");
-		
-		parentElement = wel.findElement(By.xpath("./../.."));
-		System.out.println("할머니의 태그 값 : [ "+ parentElement.getTagName() +" ]");
-		
-		//JavascriptExecutor를 이용한 방법
-		List<WebElement> welist = driver.findElements(By.tagName("td"));
-		for(WebElement element : welist) {
-			if(element.getText().equals("John")) {
-				wel = element;
-				System.out.println(element.getText());
-				break;
-			}
-		}
-		
-		JavascriptExecutor executor = (JavascriptExecutor)driver;
-		parentElement = (WebElement)executor.executeScript("return arguments[0].parentNode;", wel);
-		System.out.println("부모의 태그 값 : [ "+ parentElement.getTagName() +" ]");
-		
-		parentElement = (WebElement)executor.executeScript("return arguments[0].parentNode;", parentElement);
-		System.out.println("할머니의 태그 값 : [ "+ parentElement.getTagName() +" ]");
 		
 	}
 	
